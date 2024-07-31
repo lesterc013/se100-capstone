7d47f0f3adcda33380f0fde075d2f23afec1d5ee
lester <lesterc013@gmail.com>
2024-07-31 16:22:45 +0800

    useEffect runs on render and when a new stock is added - which gets most current price for all stocks so far

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -7,37 +7,49 @@ function AppTest() {
   const [stockSymbol, setStockSymbol] = useState('')
   const [quantity, setQuantity] = useState('')
   const [purchasePrice, setPurchasePrice] = useState('')
+  const [stockSymbolList, setStockSymbolList] = useState([])
   const [stockList, setStockList] = useState([])
 
   // useCallback with empty array - means it will only be created on first render and then memoized to prevent recreation when component re-renders -- optimization
   const getCurrentPrice = useCallback(async stockSymbol => {
     try {
-      const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${thisStockSymbol}&apikey=${
+      const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${
         import.meta.env.VITE_API_KEY_DEMO
       }`
       const response = await fetch(url)
       const jsonResponse = await response.json()
       const currentPrice = jsonResponse['Global Quote']['05. price']
+      return currentPrice
     } catch (error) {
       console.log(error.message)
       return
     }
   }, [])
 
+  // Everytime a new symbol is added to stockSymbolList i.e. stockSymbolList changes, useEffect triggers - which will loop through all the stock symbols, and make API call to get its latest price. All the latest prices are updated in stockList state variable
+  useEffect(() => {
+    const updateStockList = async () => {
+      const updatedStockList = []
+      for (const symbol of stockSymbolList) {
+        const currentPrice = await getCurrentPrice(symbol)
+        const newStock = {
+          symbol,
+          currentPrice,
+        }
+        updatedStockList.push(newStock)
+      }
+      setStockList(updatedStockList)
+    }
+    updateStockList()
+  }, [stockSymbolList, getCurrentPrice])
+
   const handleSubmit = async event => {
     event.preventDefault()
-    // !stockList.includes(stockSymbol) &&
-    //   setStockList(stockList.concat(stockSymbol))
     setStockSymbol('')
     setQuantity('')
     setPurchasePrice('')
-    !stockList.find(stock => stock.symbol === stockSymbol) &&
-      setStockList(
-        stockList.concat({
-          symbol: stockSymbol,
-          currentPrice: null,
-        })
-      )
+    !stockSymbolList.includes(stockSymbol) &&
+      setStockSymbolList(stockList.concat(stockSymbol))
   }
 
   return (
