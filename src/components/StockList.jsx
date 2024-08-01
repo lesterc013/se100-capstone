39501a85ec193302de7670a3f6084e0b61c7d671
lester <lesterc013@gmail.com>
2024-08-01 16:36:19 +0800

    StockList now uses useContext to obtain the stockList values

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { StockListContext } from './StockListContext'
 import Stock from './Stock'
 
-const StockList = ({ stockList }) => {
+const StockList = () => {
+  const stockList = useContext(StockListContext)
   return stockList.map(stock => <Stock key={stock.id} stockRecord={stock} />)
 }
 
