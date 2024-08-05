fc425523d72a5834ecf7f903e75bdd9637e2779f
lester <lesterc013@gmail.com>
2024-08-05 15:49:56 +0800

    Floating animation for StockForm input labels. Justified to center. Responsive layout for small and lg screens

--- src/components/StockForm.jsx
+++ src/components/StockForm.jsx
@@ -11,46 +11,67 @@ const StockForm = props => {
 
   return (
     <form
-      className='flex flex-col items-center md:flex-row md:justify-center'
+      className='flex flex-col items-center lg:flex-row lg:justify-center'
       onSubmit={handleSubmit}
     >
-      <div className='flex w-8/12 h-10 my-4 max-w-64'>
-        <label htmlFor='stockSymbol'>Stock</label>
+      <div className='relative flex justify-center w-8/12 h-10 my-4 max-w-64'>
         <input
+          className='peer border-b border-neutral-500 placeholder-transparent focus:outline-none'
           name='stockSymbol'
           id='stockSymbol'
           value={stockSymbol}
           type='text'
           onChange={event => setStockSymbol(event.target.value.toUpperCase())}
-          className='border-b-2 border-neutral-500'
+          placeholder='Stock'
+          autoComplete='off'
         />
+        <label
+          className='absolute left-9 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm transition-all'
+          htmlFor='stockSymbol'
+        >
+          Stock
+        </label>
       </div>
 
-      <div className='flex w-8/12 justify-between h-10 my-4 max-w-64'>
-        <label htmlFor='quantity'>Quantity</label>
+      <div className='relative flex justify-center w-8/12 h-10 my-4 max-w-64'>
         <input
+          className='peer border-b border-neutral-500 placeholder-transparent focus:outline-none'
           name='quantity'
           id='quantity'
           value={quantity}
           type='number'
           onChange={event => setQuantity(event.target.value)}
-          className='border-b-2 border-neutral-500'
+          placeholder='Quantity'
+          autoComplete='off'
         />
+        <label
+          className='absolute left-9 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm transition-all'
+          htmlFor='quantity'
+        >
+          Quantity
+        </label>
       </div>
 
-      <div className='flex w-8/12 justify-between h-10 my-4 max-w-64'>
-        <label htmlFor='purchasePrice'>Bought At</label>
+      <div className='relative flex justify-center w-8/12 h-10 my-4 max-w-64'>
         <input
+          className='peer border-b border-neutral-500 placeholder-transparent focus:outline-none'
           name='purchasePrice'
           id='purchasePrice'
           value={purchasePrice}
           type='number'
           onChange={event => setPurchasePrice(event.target.value)}
-          className='border-b-2 border-neutral-500'
+          placeholder='Purchase Price'
+          autoComplete='off'
         />
+        <label
+          className='absolute left-9 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm transition-all'
+          htmlFor='purchasePrice'
+        >
+          Bought At
+        </label>
       </div>
       <button
-        className='border border-blue-600 text-blue-600 bg-transparent hover:bg-blue-600 hover:border-transparent hover:text-white active:bg-blue-700 min-w-16 rounded-full'
+        className='border border-blue-600 text-blue-600 bg-transparent hover:bg-blue-600 hover:border-transparent hover:text-white active:bg-blue-700 min-w-16 rounded-full my-4'
         type='submit'
       >
         Add
