fa416c902b79abcfbea4dd390d5fe7ca70140218
lester <lesterc013@gmail.com>
2024-08-14 14:52:24 +0800

    Included money-icon svg as tab icon and in the MainTitle

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -2,9 +2,19 @@ const MainTitle = () => {
   return (
     <div className='flex w-full justify-center'>
       <div className='flex justify-center lg:w-3/4 lg:justify-start'>
-        <h1 className='font-rubik text-xl font-bold dark:text-neutral-300'>
+        <img
+          src='../src/assets/money-icon.svg'
+          alt='money-icon'
+          className='h-8 w-8'
+        ></img>
+        <h1 className='font-rubik text-xl font-bold dark:text-neutral-300 px-4'>
           Your Financial Overview
         </h1>
+        <img
+          src='../src/assets/money-icon.svg'
+          alt='money-icon'
+          className='h-8 w-8'
+        ></img>
       </div>
     </div>
   )
